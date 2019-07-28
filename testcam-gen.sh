fad4b94612595f8e4fcf579e7ce31f2b068f6d34
Ken Sloat <ken.sloat@ohmlinxelectronics.com>
2019-07-28 09:34:34 -0400

    Add quick dirty script for launching two fake camera devices.

--- testcam-gen.sh
+++ testcam-gen.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "hello world"
+# Kill any other gstreamer proccesses
+pkill -f gst
+
+# Make sure module is no loaded already
+sudo modprobe -r v4l2loopback
+
+sleep 1
+
+# Load module with 2 camera sources
+sudo modprobe v4l2loopback devices=2
+
+# Launch our fake video feeds
+gst-launch-1.0 videotestsrc pattern=ball ! v4l2sink device=/dev/video0 &
+gst-launch-1.0 videotestsrc ! v4l2sink device=/dev/video1 &
